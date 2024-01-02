7918c0eccb99baa47034dff691d91f46687da83a
Armen <armen.nsk@gmail.com>
2024-01-01 20:29:43 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MyFun from './components/MyFun';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hW Hello World ex16</h1> 
+  <MyFun/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyFun.js
@@ -0,0 +1,7 @@
+const MyFun = () => {
+    return ( 
+        <h1>My home work Hello World!</h1>
+     );
+}
+ 
+export default MyFun;
